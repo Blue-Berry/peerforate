e0ac8de5e8fbbe64f275ba7dbc2248408e7878b4
Liam Berry <liamandberry@gmail.com>
2025-11-28 13:01:46 +0200

    fix some bugs

--- server/main.ml
+++ server/main.ml
@@ -10,10 +10,6 @@ module K = Wglib.Wgapi.Key
 (*   - Prevent DoS *)
 (*   - Refactor *)
 (*   - Add tests *)
-(*   - Create DNS server *)
-(*   - DNS server forwarding? *)
-(*   - Create DNS client *)
-(*   - Publish public key to DNS TXT record *)
 (*   - Switch to ipv6 *)
 
 let int_to_hex i = Printf.sprintf "%02x" i
@@ -39,41 +35,49 @@ let main ~net =
   while true do
     let client_addr, len = Eio.Net.recv sock buf in
     Logs.info (fun m ->
-      m "Received %d bytes from %a\n%!" len Eio.Net.Sockaddr.pp client_addr);
-    let packet = P.of_cstruct ~hdr:false buf in
-    let hst_key = P.copy_t_hst_key packet |> K.of_string in
-    let dest_key = P.copy_t_dest_key packet |> K.of_string in
-    let timestamp = P.get_t_timestamp packet in
-    let mac = P.copy_t_mac packet |> string_to_hex in
-    Logs.info (fun m ->
       m
-        "Version: %d\nHost key: %s\nDest key %s\nTimestamp: %s\nMAC: %s"
-        (P.get_t_version packet)
-        (hst_key |> K.to_base64_string)
-        (dest_key |> K.to_base64_string)
-        (Int64.to_string_hum timestamp)
-        mac);
-    let mac =
-      P.gen_mac
-        ~pub_key:(hst_key |> K.to_string)
-        ~priv_key:Wg_nat.Crypto.rng_priv_key
-        ~hst_key:(K.to_string hst_key)
-        ~dest_key:(K.to_string dest_key)
-        timestamp
-      |> string_to_hex
-    in
-    let now =
-      Ptime.v (Ptime_clock.now_d_ps ()) |> Ptime.to_span |> Ptime.Span.to_float_s
-    in
-    let timestamp = Int64.to_float timestamp in
-    let age = now -. timestamp in
-    if Float.(age < Config.max_message_age_s)
-    then (
-      Logs.info (fun m -> m "Generated MAC %s" mac);
-      let reply = R.create ~hst_key ~dest_key () |> R.to_cstruct ~hdr:false in
-      Eio.Net.send sock [ reply ] ~dst:client_addr;
-      Logs.info (fun m -> m "Sent reply to %a\n" Eio.Net.Sockaddr.pp client_addr))
-    else Logs.info (fun m -> m "Message too old")
+        "Received %d bytes from %a; Expected: %d\n"
+        len
+        Eio.Net.Sockaddr.pp
+        client_addr
+        P.payload_size);
+    if len < P.payload_size
+    then Logs.info (fun m -> m "Invalid packetsize %d" len)
+    else (
+      let packet = P.of_cstruct ~hdr:false buf in
+      let hst_key = P.copy_t_hst_key packet |> K.of_string in
+      let dest_key = P.copy_t_dest_key packet |> K.of_string in
+      let timestamp = P.get_t_timestamp packet in
+      let mac = P.copy_t_mac packet |> string_to_hex in
+      Logs.info (fun m ->
+        m
+          "Version: %d\nHost key: %s\nDest key %s\nTimestamp: %s\nMAC: %s"
+          (P.get_t_version packet)
+          (hst_key |> K.to_base64_string)
+          (dest_key |> K.to_base64_string)
+          (Int64.to_string_hum timestamp)
+          mac);
+      let mac =
+        P.gen_mac
+          ~pub_key:(hst_key |> K.to_string)
+          ~priv_key:Wg_nat.Crypto.rng_priv_key
+          ~hst_key:(K.to_string hst_key)
+          ~dest_key:(K.to_string dest_key)
+          timestamp
+        |> string_to_hex
+      in
+      let now =
+        Ptime.v (Ptime_clock.now_d_ps ()) |> Ptime.to_span |> Ptime.Span.to_float_s
+      in
+      let timestamp = Int64.to_float timestamp in
+      let age = now -. timestamp in
+      if Float.(age < Config.max_message_age_s) && Float.(age >= 0.)
+      then (
+        Logs.info (fun m -> m "Generated MAC %s" mac);
+        let reply = R.create ~hst_key ~dest_key () |> R.to_cstruct ~hdr:false in
+        Eio.Net.send sock [ reply ] ~dst:client_addr;
+        Logs.info (fun m -> m "Sent reply to %a\n" Eio.Net.Sockaddr.pp client_addr))
+      else Logs.info (fun m -> m "Message too old"))
   done
 ;;
 
